c12f6805772107bef972426f9a8cd30f65010cc5
sangeetha <sangeetha.raju@syncfusion.com>
2019-01-11 19:11:01 +0530

    Attached the sample

---
+++ GettingStarted/GettingStarted/GettingStarted/Model/BookInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted
+{
+    public class BookInfo : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string bookName;
+        private string bookDesc;
+
+        #endregion
+
+        #region Constructor
+
+        public BookInfo()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string BookName
+        {
+            get { return bookName; }
+            set
+            {
+                bookName = value;
+                OnPropertyChanged("BookName");
+            }
+        }
+
+        public string BookDescription
+        {
+            get { return bookDesc; }
+            set
+            {
+                bookDesc = value;
+                OnPropertyChanged("BookDescription");
+            }
+        }
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
